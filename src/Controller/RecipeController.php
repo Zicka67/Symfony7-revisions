17d4a0213b6dec78b503789f2bdac3ad1a8448e2
Zicka67 <art.book@hotmail.fr>
2024-03-09 17:35:12 +0100

    Revision pour les return render ou response + twig

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -14,7 +14,8 @@ class RecipeController extends AbstractController
     #[Route('/recette', name: 'recipe.index')]
     public function index(Request $request): Response
     {
-        return new Response('Recette');
+        // return new Response('Recette');
+        return $this->render('recipe/index.html.twig');
     }
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug'=> '[a-z0-9-]+'])]
