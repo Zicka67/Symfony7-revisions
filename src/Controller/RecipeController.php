f13b81bc0d7bd2cf661967d772a911f4b908a7c3
Zicka67 <art.book@hotmail.fr>
2024-03-11 17:08:48 +0100

    Modification du recipeController pour afficher les recettes dans une boucle et twig dans la vue pour l'affichage

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,25 +13,32 @@ class RecipeController extends AbstractController
 {
 
     #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
+        $recipes = $repository->findAll();
+        // dd($recipes);
         // return new Response('Recette');
-        return $this->render('recipe/index.html.twig');
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
     }
 
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug'=> '[a-z0-9-]+'])]
-    public function show(Request $request, $slug, $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
         // dd($request->attributes->get('slug'), $request->attributes->get('id'));
         // dd($slug, $id);
         // return new JsonResponse([
         //     'slug' => $slug
         // ]);
-        
+        $recipe = $repository->find($id);
+        if($recipe->getSlug() != $slug) {
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId() ]);
+        }
         // return new Response('Recette des : ' . $slug);
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id
+            'recipe' => $recipe
+        
         ]);
     }
 }
