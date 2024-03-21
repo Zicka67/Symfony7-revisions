cc0d3206783add435eaf1d49c84fc4bf93906dc0
Zicka67 <art.book@hotmail.fr>
2024-03-21 17:32:38 +0100

    Modification dans le recipe et home controller

---
+++ src/Controller/Admin/RecipeController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+#[Route('/admin/recettes', name: 'admin.recipe.')]
+class RecipeController extends AbstractController
+{
+
+    #[Route('/', name: 'index')]
+    public function index(RecipeRepository $repository): Response
+    {
+        $recipes = $repository->findWithDurationLowerThan(20);
+        $recipeTotalDuration = $repository->findTotalDuration();
+
+        return $this->render('admin/recipe/index.html.twig', [
+            'recipes' => $recipes,
+            'recipeTotalDuration' => $recipeTotalDuration
+        ]);
+    }
+
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $recipe = new Recipe();
+  
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $recipe->setCreatedAt(new \DateTimeImmutable());
+            $recipe->setUpdatedAt(new \DateTimeImmutable());
+            $em->persist($recipe);
+            $em->flush();
+            $this->addFlash('success', 'La recette a bien été crée');
+            return $this->redirectToRoute('admin.recipe.index');
+        }
+
+        return $this->render('admin/recipe/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}', name: 'edit', methods: ['GET', 'POST'], requirements: ['id' => Requirement::DIGITS])]
+    public function edit(Recipe $recipe, Request $request, EntityManagerInterface $em): Response
+    {
+  
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $recipe->setCreatedAt(new \DateTimeImmutable());
+            $recipe->setUpdatedAt(new \DateTimeImmutable());
+            $em->flush();
+            $this->addFlash('success', 'La recette a bien été modifiée');
+            return $this->redirectToRoute('admin/recipe.index');
+        }
+
+        return $this->render('admin/recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form
+        ]);
+    }
+    
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'], requirements: ['id' => Requirement::DIGITS])]
+    public function remove(Recipe $recipe, EntityManagerInterface $em)
+    {
+        $em->remove($recipe);
+        $em->flush();
+        $this->addFlash('success', 'La recette a bien été suprimée');
+
+        return $this->redirectToRoute('admin/recipe.index');
+    }
+
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use SebastianBergmann\CodeCoverage\Report\Html\Renderer;
+use App\Repository\RecipeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -20,4 +20,36 @@ class HomeController extends AbstractController
         // return new Response('Bonjour ' . $request->query->get('name', 'Inconnu'));
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/recettes', name: 'show')]
+    public function show(RecipeRepository $repository): Response
+    {
+    
+        $recipe = $repository->findAll();
+        $recipeTotalDuration = $repository->findTotalDuration();
+        // dd($recipe);
+        
+        return $this->render('recipe/show.html.twig', [
+            'recipes' => $recipe,
+            'recipeTotalDuration' => $recipeTotalDuration
+        
+        ]);
+    }
+
+    #[Route('/recettes/{slug}', name: 'recipe.detail')]
+    public function details(string $slug, RecipeRepository $repository): Response
+    {
+    
+        $recipe = $repository->findOneBy(['slug' => $slug]);
+        
+        if (!$recipe) {
+            throw $this->createNotFoundException('La recette demandée n\'existe pas.');
+        }
+        
+        return $this->render('recipe/details.html.twig', [
+            'recipe' => $recipe
+        
+        ]);
+    }
+
 }
