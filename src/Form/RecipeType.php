d27dcd18eea121309d57a19f6eda89cfa1926326
Zicka67 <art.book@hotmail.fr>
2024-03-22 14:40:28 +0100

    clean

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -12,8 +12,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\String\Slugger\AsciiSlugger;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\Regex;
 
 class RecipeType extends AbstractType
 {
